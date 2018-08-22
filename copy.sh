857c045c2227c6cc77118e6f9251ef6f306587da
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-08-22 18:39:00 -0500

    Creando archivos

---
+++ copy.sh
@@ -0,0 +1,10 @@
+#git clone https://github.com/abdiasriver/avilinux
+
+#git clone --recursive https://github.com/abdiasriver/arch-awesome
+#git clone --recursive https://github.com/lcpz/awesome-copycats.git
+
+#mv -bv awesome/* ~/.config/awesome; rm -rf awesome-copycats
+
+
+#rmdir -p nombre_carpeta
+#rm -R nombre_carpeta
