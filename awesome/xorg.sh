261b5b39f828bf6f6f4ac147f96fa972599f4008
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-19 01:01:21 -0500

    Update xorg.sh

--- awesome/xorg.sh
+++ awesome/xorg.sh
@@ -6,7 +6,7 @@ echo "####                                                    ####"
 echo "################################################################"
 
 
-sudo pacman -S bc rsync termite thunar dmenu mlocate bash-completion pkgstats zip unzip unrar p7zip lzop cpio alsa-utils alsa-plugins pulseaudio pulseaudio-alsa ntfs-3g dosfstools exfat-utils f2fs-tools fuse fuse-exfat autofs mtpfs 
+sudo pacman -S bc scrot rsync termite thunar dmenu mlocate bash-completion pkgstats zip unzip unrar p7zip lzop cpio alsa-utils alsa-plugins pulseaudio pulseaudio-alsa ntfs-3g dosfstools exfat-utils f2fs-tools fuse fuse-exfat autofs mtpfs 
 sudo pacman -S --asdeps --needed cairo fontconfig freetype2
 sudo pacman -S xorg-server xorg-apps xorg-server-xwayland xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput
 sudo pacman -S mesa
