4288155a0155199d910e399df61c2c85ff6a14c7
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-25 20:37:48 -0500

    Update awesome.sh

--- awesome/awesome.sh
+++ awesome/awesome.sh
@@ -11,9 +11,11 @@ sudo pacman -S awesome lxappearance leafpad epdfview nitrogen variety
 mkdir -p ~/.config/awesome/
 cp -r awconf/* ~/.config/awesome
 cp .xinitrc ~/.xinitrc
+cp .bashrc ~/.bashrc
+cp .zshrc ~/.zshrc
 
 
 ## LIGHTDM
 
-sudo pacman -S lightdm lightdm-gtk-greeter 
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
 sudo systemctl enable lightdm.service -f
