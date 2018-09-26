6e070cec6142cf1851a85655ddfe234d88aa4f76
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-09-26 15:04:30 -0500

    actualizacion

--- awesome/apps.sh
+++ awesome/apps.sh
@@ -5,19 +5,17 @@ echo "#######   APPS QUE TENGO, LA MAYORIA               #####"
 echo "#######    PUEDE YA ESTAR INSTALADA POR EL SISTEMA #####" 
 echo "########################################################"
 
-trizen -S adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arj autofs awesome awesome-terminal-fonts axel ms-office-online
+trizen -S adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arj autofs awesome-terminal-fonts axel ms-office-online
 
 trizen -S bash-completion btrfs-progs 
 
-trizen -S cabextract cdrdao cdrtools compton conky-lua-archers cower cpio discord 
-
 trizen -S dmenu dmidecode dnsmasq downgrade
 
-trizen -S epdfview exfat-utils expac 
+trizen -S epdfview exfat-utils
 
 trizen -S f2fs-tools ffmpegthumbnailer ffmpegthumbs file-roller font-manager 
 
-trizen -S git gnome-defaults-list gnome-disk-utility gnome-keyring gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
+trizen -S git gnome-disk-utility gnome-keyring gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
 
 trizen -S hardcode-fixer-git hardinfo hddtemp htop
 
@@ -25,32 +23,34 @@ trizen -S imagemagick intel-tbb intel-ucode inxi ipw2100-fw ipw2200-fw
 
 trizen -S jre10-openjdk 
 
-trizen -S lib32-mesa lib32-mesa-vdpau libdvdcss libva-intel-driver libva-vdpau-driver libvdpau-va-gl lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings linux-headers lsb-release lxappearance lzop
+trizen -S lib32-mesa lib32-mesa-vdpau libdvdcss libva-intel-driver libva-vdpau-driver libvdpau-va-gl linux-headers lsb-release lxappearance lzop
 
-trizen -S mesa-vdpau mlocate mpc mpd mtpfs mutter 
+trizen -S mesa-vdpau mpc mpd mtpfs mutter 
 
-trizen -S ncmpcpp neofetch network-manager-applet networkmanager networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc nitrogen noto-fonts ntfs-3g numix-circle-icon-theme-git numlockx 
+trizen -S ncmpcpp neofetch network-manager-applet networkmanager networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc noto-fonts ntfs-3g numix-circle-icon-theme-git numlockx 
 
 trizen -S p7zip papirus-icon-theme-git pavucontrol perl-linux-desktopfiles pkgstats python-pywal
 
 trizen -S qt5-declarative
 
-trizen -S rsync rxvt-unicode
+trizen -S rxvt-unicode
 
 trizen -S sardi-colora-variations-icons-git sardi-flat-colora-variations-icons-git sardi-flat-mixing-icons-git sardi-flexible-colora-variations-icons-git sardi-flexible-luv-colora-variations-icons-git sardi-flexible-mixing-icons-git sardi-flexible-variations-icons-git sardi-ghost-flexible-colora-variations-icons-git sardi-ghost-flexible-mixing-icons-git sardi-ghost-flexible-variations-icons-git sardi-icons sardi-mixing-icons-git sardi-mono-colora-variations-icons-git sardi-mono-mixing-icons-git sardi-mono-numix-colora-variations-icons-git sardi-mono-papirus-colora-variations-icons-git sardi-orb-colora-mixing-icons-git sardi-orb-colora-variations-icons-git screenfetch scrot sharutils simplescreenrecorder sublime-text-dev surfn-icons-git
 
 trizen -S tamsyn-font temps terminus-font termite thunar thunar-archive-plugin thunar-volman transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-droid ttf-font-awesome ttf-inconsolata ttf-liberation ttf-mac-fonts ttf-roboto ttf-ubuntu-font-family
 
-trizen -S unace unrar unzip urxvt-perls urxvt-resize-font-git uudeview 
+trizen -S unace unrar unzip urxvt-perls urxvt-resize-font-git 
 
 trizen -S variety vicious vlc
 
-trizen -S weston wget
+trizen -S wget
 
 trizen -S xcursor-breeze 
-trizen -S xorg-iceauth xorg-luit xorg-server xorg-server-xwayland xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwd xorg-xwininfo xorg-xwud
 
-trizen -S yajl
+
+
+#trizen -S xorg-iceauth xorg-luit xorg-server xorg-server-xwayland xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwd xorg-xwininfo xorg-xwud
+
 
 trizen -S zip zramswap zsh zsh-completions zsh-syntax-highlighting
 
--- awesome/shr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-mkdir -p imgs
-sudo cp -r awesome.sh ~/arch-awesome/awesome/imgs/
