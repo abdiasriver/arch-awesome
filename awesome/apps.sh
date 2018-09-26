cc87507063a1b2a3a2d77e2fc8fa312aa7b08b74
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-25 20:18:28 -0500

    Update apps.sh

--- awesome/apps.sh
+++ awesome/apps.sh
@@ -1,65 +1,53 @@
+adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arcolinux-arc-themes-git arcolinux-awesome-git arcolinux-bin-git arcolinux-config-git arcolinux-faces-git arcolinux-fonts-git arcolinux-neofetch-git arcolinux-nitrogen-git arcolinux-oblogout arcolinux-oblogout-themes-git arcolinux-root-git arcolinux-slim arcolinux-slimlock-themes-git arcolinux-termite-themes-git arcolinux-variety-git arcolinux-wallpapers-git arcolinux-xfce-thunar-git arj autofs awesome awesome-terminal-fonts axel 
 
-4kvideodownloader adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arch-wiki-lite arcolinux-arc-themes-git arcolinux-awesome-git arcolinux-bin-git arcolinux-config-git arcolinux-faces-git arcolinux-fonts-git arcolinux-neofetch-git arcolinux-nitrogen-git arcolinux-oblogout arcolinux-oblogout-themes-git arcolinux-root-git arcolinux-slim arcolinux-slimlock-themes-git arcolinux-termite-themes-git arcolinux-variety-git arcolinux-wallpapers-git arcolinux-xfce-thunar-git arj autofs awesome awesome-terminal-fonts axel 
-
-bash-completion blender btrfs-progs 
+bash-completion btrfs-progs 
 
 cabextract cdrdao cdrtools compton conky-lua-archers cower cpio discord 
 
 dmenu dmidecode dnsmasq downgrade
+
 epdfview exfat-utils expac 
+
 f2fs-tools ffmpegthumbnailer ffmpegthumbs file-roller font-manager 
-git gnome-defaults-list gnome-disk-utility gnome-keyring google-chrome gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
-hardcode-fixer-git hardinfo hddtemp htop hugo 
+
+git gnome-defaults-list gnome-disk-utility gnome-keyring gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
+
+hardcode-fixer-git hardinfo hddtemp htop
+
 imagemagick intel-tbb intel-ucode inxi ipw2100-fw ipw2200-fw 
+
 jre10-openjdk 
+
 lib32-mesa lib32-mesa-vdpau libdvdcss libva-intel-driver libva-vdpau-driver libvdpau-va-gl lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings linux-headers lsb-release lxappearance lzop
+
 mesa-vdpau mlocate mpc mpd mtpfs mutter 
+
 ncmpcpp neofetch network-manager-applet networkmanager networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc nitrogen noto-fonts ntfs-3g numix-circle-icon-theme-git numlockx 
 
-p7zip papirus-icon-theme-git pavucontrol perl-linux-desktopfiles pkgstats popcorntime python-pywal
+p7zip papirus-icon-theme-git pavucontrol perl-linux-desktopfiles pkgstats python-pywal
+
 qt5-declarative
 
 rsync rxvt-unicode
+
 sardi-colora-variations-icons-git sardi-flat-colora-variations-icons-git sardi-flat-mixing-icons-git sardi-flexible-colora-variations-icons-git sardi-flexible-luv-colora-variations-icons-git sardi-flexible-mixing-icons-git sardi-flexible-variations-icons-git sardi-ghost-flexible-colora-variations-icons-git sardi-ghost-flexible-mixing-icons-git sardi-ghost-flexible-variations-icons-git sardi-icons sardi-mixing-icons-git sardi-mono-colora-variations-icons-git sardi-mono-mixing-icons-git sardi-mono-numix-colora-variations-icons-git sardi-mono-papirus-colora-variations-icons-git sardi-orb-colora-mixing-icons-git sardi-orb-colora-variations-icons-git screenfetch scrot sharutils simplescreenrecorder sublime-text-dev surfn-icons-git
 
-tamsyn-font temps terminus-font termite thunar thunar-archive-plugin thunar-volman transmission-gtk trizen ttf-bitstream-vera ttf-dejavu ttf-droid ttf-font-awesome ttf-inconsolata ttf-liberation ttf-mac-fonts ttf-roboto ttf-ubuntu-font-family
-unace unrar
-unzip urxvt-perls urxvt-resize-font-git uudeview variety
-vicious vlc
+tamsyn-font temps terminus-font termite thunar thunar-archive-plugin thunar-volman transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-droid ttf-font-awesome ttf-inconsolata ttf-liberation ttf-mac-fonts ttf-roboto ttf-ubuntu-font-family
+
+unace unrar unzip urxvt-perls urxvt-resize-font-git uudeview 
+
+variety vicious vlc
+
 weston wget
-xclip xcursor-breeze
-xdg-user-dirs-gtk
-xf86-input-keyboard
-xf86-video-intel
-xorg-iceauth
-xorg-luit
-xorg-server
-xorg-server-xwayland
-xorg-sessreg
-xorg-smproxy
-xorg-x11perf
-xorg-xbacklight
-xorg-xcmsdb
-xorg-xcursorgen
-xorg-xdriinfo
-xorg-xev
-xorg-xgamma
-xorg-xhost
-xorg-xinit
-xorg-xinput
-xorg-xkbevd
-xorg-xkbutils
-xorg-xkill
-xorg-xlsatoms
-xorg-xlsclients
-xorg-xpr
-xorg-xrefresh
-xorg-xset
-xorg-xsetroot
-xorg-xvinfo
-xorg-xwd
-xorg-xwininfo
-xorg-xwud
-yajl yaourt youtube-dl
-zip
-zramswap zsh zsh-completions zsh-syntax-highlighting
+
+xcursor-breeze 
+xorg-iceauth xorg-luit xorg-server xorg-server-xwayland xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwd xorg-xwininfo xorg-xwud
+
+yajl
+
+zip zramswap zsh zsh-completions zsh-syntax-highlighting
+
+
+
+
+# xdg-user-dirs-gtk xf86-input-keyboard xf86-video-intel
