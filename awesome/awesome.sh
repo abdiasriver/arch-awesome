4dd47f057d5f1ea09f5b2547560b6b19daf5ff33
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-09-25 22:43:53 -0500

    Actualizacion total

--- awesome/awesome.sh
+++ awesome/awesome.sh
@@ -15,9 +15,15 @@ cp .bashrc ~/.bashrc
 cp .zshrc ~/.zshrc
 cp .face ~/.face
 
+sudo mkdir -p /usr/share/backgrounds/arcolinux/
+sudo mkdir -p /usr/share/pixmaps/faces/
+sudo cp -r imgs/icy-yosemite.jpg /usr/share/backgrounds/arcolinux/
+sudo cp -r imgs/arcolinux-golden.png /usr/share/pixmaps/faces/
+
 ## LIGHTDM
 
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings 
+sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings sardi-mono-colora-variations-icons-git noto-fonts
 sudo systemctl enable lightdm.service -f
+sudo mkdir -p /etc/lightdm/
 sudo cp -r lightdm/* /etc/lightdm/
 
---
+++ awesome/shr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -e
+mkdir -p imgs
+sudo cp -r awesome.sh ~/arch-awesome/awesome/imgs/
