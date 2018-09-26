7202a24cab53dbfb2193b5185eef0ce71b1db9a7
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-25 19:33:46 -0500

    Update xorg.sh

--- awesome/xorg.sh
+++ awesome/xorg.sh
@@ -5,10 +5,11 @@ echo "####             Instalacion Xorg+ axeso.red            ####"
 echo "####                                                    ####"
 echo "################################################################"
 
-
+sudo pacman -S xdg-user-dirs mesa mesa-libgl libvdpau-va-gl lib32-mesa 
 sudo pacman -S bc scrot rsync termite thunar dmenu mlocate bash-completion pkgstats zip unzip unrar p7zip lzop cpio alsa-utils alsa-plugins pulseaudio pulseaudio-alsa ntfs-3g dosfstools exfat-utils f2fs-tools fuse fuse-exfat autofs mtpfs 
 sudo pacman -S --asdeps --needed cairo fontconfig freetype2
 sudo pacman -S xorg-server xorg-apps xorg-server-xwayland xorg-xinit xorg-xkill xorg-xinput xf86-input-libinput
-sudo pacman -S mesa
 sudo pacman -S networkmanager dnsmasq network-manager-applet nm-connection-editor 
 sudo systemctl enable NetworkManager.service 
+
+#sudo pacman -S xf86-video-intel 
