38b187185520f79f68c4f5f4300bd2e068b88bb0
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-25 20:58:08 -0500

    Update 3d-party.sh

--- awesome/3d-party.sh
+++ awesome/3d-party.sh
@@ -31,4 +31,4 @@ echo "##################################################################"
 echo "###                  REPO arcolinux AGREGADO                  ####"
 echo "##################################################################"
 
-trizen -S arcolinux-arc-themes-git arcolinux-awesome-git arcolinux-bin-git arcolinux-config-git arcolinux-faces-git arcolinux-fonts-git arcolinux-neofetch-git arcolinux-nitrogen-git arcolinux-oblogout arcolinux-oblogout-themes-git arcolinux-root-git arcolinux-slim arcolinux-slimlock-themes-git arcolinux-termite-themes-git arcolinux-variety-git arcolinux-wallpapers-git arcolinux-xfce-thunar-git
+trizen -S arcolinux-awesome-git arcolinux-bin-git arcolinux-config-git arcolinux-faces-git arcolinux-fonts-git arcolinux-neofetch-git arcolinux-nitrogen-git arcolinux-oblogout arcolinux-oblogout-themes-git arcolinux-root-git arcolinux-slim arcolinux-slimlock-themes-git arcolinux-termite-themes-git arcolinux-variety-git arcolinux-wallpapers-git
