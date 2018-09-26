9a81a444d34b3ca34a9e3270722d66a49036affa
abdias <abdias@alumnos.uaslp.edu.mx>
2018-09-25 21:25:45 -0500

    Update apps.sh

--- awesome/apps.sh
+++ awesome/apps.sh
@@ -1,53 +1,60 @@
-adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arj autofs awesome awesome-terminal-fonts axel 
+#!/bin/bash
+set -e
+echo "########################################################"
+echo "#######   APPS QUE TENGO, LA MAYORIA               #####"
+echo "#######    PUEDE YA ESTAR INSTALADA POR EL SISTEMA #####" 
+echo "########################################################"
 
-bash-completion btrfs-progs 
+trizen -S adobe-source-sans-pro-fonts alsa-utils arandr arc-gtk-theme arj autofs awesome awesome-terminal-fonts axel 
 
-cabextract cdrdao cdrtools compton conky-lua-archers cower cpio discord 
+trizen -S  bash-completion btrfs-progs 
 
-dmenu dmidecode dnsmasq downgrade
+trizen -S  cabextract cdrdao cdrtools compton conky-lua-archers cower cpio discord 
 
-epdfview exfat-utils expac 
+trizen -S dmenu dmidecode dnsmasq downgrade
 
-f2fs-tools ffmpegthumbnailer ffmpegthumbs file-roller font-manager 
+trizen -S epdfview exfat-utils expac 
 
-git gnome-defaults-list gnome-disk-utility gnome-keyring gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
+trizen -S f2fs-tools ffmpegthumbnailer ffmpegthumbs file-roller font-manager 
 
-hardcode-fixer-git hardinfo hddtemp htop
+trizen -S git gnome-defaults-list gnome-disk-utility gnome-keyring gst-libav gst-plugins-bad gst-plugins-ugly gthumb gtk-engine-murrine gtk2-perl gvfs gvfs-afc gvfs-goa gvfs-google gvfs-mtp 
 
-imagemagick intel-tbb intel-ucode inxi ipw2100-fw ipw2200-fw 
+trizen -S hardcode-fixer-git hardinfo hddtemp htop
 
-jre10-openjdk 
+trizen -S imagemagick intel-tbb intel-ucode inxi ipw2100-fw ipw2200-fw 
 
-lib32-mesa lib32-mesa-vdpau libdvdcss libva-intel-driver libva-vdpau-driver libvdpau-va-gl lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings linux-headers lsb-release lxappearance lzop
+trizen -S jre10-openjdk 
 
-mesa-vdpau mlocate mpc mpd mtpfs mutter 
+trizen -S lib32-mesa lib32-mesa-vdpau libdvdcss libva-intel-driver libva-vdpau-driver libvdpau-va-gl lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings linux-headers lsb-release lxappearance lzop
 
-ncmpcpp neofetch network-manager-applet networkmanager networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc nitrogen noto-fonts ntfs-3g numix-circle-icon-theme-git numlockx 
+trizen -S mesa-vdpau mlocate mpc mpd mtpfs mutter 
 
-p7zip papirus-icon-theme-git pavucontrol perl-linux-desktopfiles pkgstats python-pywal
+trizen -S ncmpcpp neofetch network-manager-applet networkmanager networkmanager-openconnect networkmanager-openvpn networkmanager-pptp networkmanager-vpnc nitrogen noto-fonts ntfs-3g numix-circle-icon-theme-git numlockx 
 
-qt5-declarative
+trizen -S p7zip papirus-icon-theme-git pavucontrol perl-linux-desktopfiles pkgstats python-pywal
 
-rsync rxvt-unicode
+trizen -S qt5-declarative
 
-sardi-colora-variations-icons-git sardi-flat-colora-variations-icons-git sardi-flat-mixing-icons-git sardi-flexible-colora-variations-icons-git sardi-flexible-luv-colora-variations-icons-git sardi-flexible-mixing-icons-git sardi-flexible-variations-icons-git sardi-ghost-flexible-colora-variations-icons-git sardi-ghost-flexible-mixing-icons-git sardi-ghost-flexible-variations-icons-git sardi-icons sardi-mixing-icons-git sardi-mono-colora-variations-icons-git sardi-mono-mixing-icons-git sardi-mono-numix-colora-variations-icons-git sardi-mono-papirus-colora-variations-icons-git sardi-orb-colora-mixing-icons-git sardi-orb-colora-variations-icons-git screenfetch scrot sharutils simplescreenrecorder sublime-text-dev surfn-icons-git
+trizen -S rsync rxvt-unicode
 
-tamsyn-font temps terminus-font termite thunar thunar-archive-plugin thunar-volman transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-droid ttf-font-awesome ttf-inconsolata ttf-liberation ttf-mac-fonts ttf-roboto ttf-ubuntu-font-family
+trizen -S sardi-colora-variations-icons-git sardi-flat-colora-variations-icons-git sardi-flat-mixing-icons-git sardi-flexible-colora-variations-icons-git sardi-flexible-luv-colora-variations-icons-git sardi-flexible-mixing-icons-git sardi-flexible-variations-icons-git sardi-ghost-flexible-colora-variations-icons-git sardi-ghost-flexible-mixing-icons-git sardi-ghost-flexible-variations-icons-git sardi-icons sardi-mixing-icons-git sardi-mono-colora-variations-icons-git sardi-mono-mixing-icons-git sardi-mono-numix-colora-variations-icons-git sardi-mono-papirus-colora-variations-icons-git sardi-orb-colora-mixing-icons-git sardi-orb-colora-variations-icons-git screenfetch scrot sharutils simplescreenrecorder sublime-text-dev surfn-icons-git
 
-unace unrar unzip urxvt-perls urxvt-resize-font-git uudeview 
+trizen -S tamsyn-font temps terminus-font termite thunar thunar-archive-plugin thunar-volman transmission-gtk ttf-bitstream-vera ttf-dejavu ttf-droid ttf-font-awesome ttf-inconsolata ttf-liberation ttf-mac-fonts ttf-roboto ttf-ubuntu-font-family
 
-variety vicious vlc
+trizen -S unace unrar unzip urxvt-perls urxvt-resize-font-git uudeview 
 
-weston wget
+trizen -S variety vicious vlc
 
-xcursor-breeze 
-xorg-iceauth xorg-luit xorg-server xorg-server-xwayland xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwd xorg-xwininfo xorg-xwud
+trizen -S weston wget
 
-yajl
+trizen -S xcursor-breeze 
+trizen -S xorg-iceauth xorg-luit xorg-server xorg-server-xwayland xorg-sessreg xorg-smproxy xorg-x11perf xorg-xbacklight xorg-xcmsdb xorg-xcursorgen xorg-xdriinfo xorg-xev xorg-xgamma xorg-xhost xorg-xinit xorg-xinput xorg-xkbevd xorg-xkbutils xorg-xkill xorg-xlsatoms xorg-xlsclients xorg-xpr xorg-xrefresh xorg-xset xorg-xsetroot xorg-xvinfo xorg-xwd xorg-xwininfo xorg-xwud
 
-zip zramswap zsh zsh-completions zsh-syntax-highlighting
+trizen -S yajl
+
+trizen -S zip zramswap zsh zsh-completions zsh-syntax-highlighting
 
 
 
 
-# xdg-user-dirs-gtk xf86-input-keyboard xf86-video-intel
+trizen -S xdg-user-dirs-gtk xf86-input-keyboard xf86-video-intel
