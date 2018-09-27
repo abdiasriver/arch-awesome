4c0997d22e8075f62e40f71decc1b8f334646357
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-09-26 23:25:04 -0500

    nuevos scripts

--- awesome/awesome.sh
+++ awesome/awesome.sh
@@ -21,9 +21,20 @@ sudo cp -r imgs/icy-yosemite.jpg /usr/share/backgrounds/arcolinux/
 sudo cp -r imgs/arcolinux-golden.png /usr/share/pixmaps/faces/
 
 ## LIGHTDM
-
-sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings sardi-mono-colora-variations-icons-git noto-fonts
+sh extras/000-use-all-cores-makepkg-conf-v2.sh
+sh extras/10-trust-key-v1.sh
+sh extras/20-add-arcolinux-repo-to-pacman-conf-v2.sh
+#sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings sardi-mono-colora-variations-icons-git noto-fonts
+#sudo systemctl enable lightdm.service -f
+#sudo mkdir -p /etc/lightdm/
+#sudo cp -r lightdm/* /etc/lightdm/
+sudo pacman -Syyu --noconfirm
+#installing displaymanager or login manager
+sudo pacman -S --noconfirm --needed lightdm
+sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
+sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git 
+#installing desktop environment
+sudo pacman -S awesome vicious --noconfirm --needed
+#enabling displaymanager or login manager
 sudo systemctl enable lightdm.service -f
-sudo mkdir -p /etc/lightdm/
-sudo cp -r lightdm/* /etc/lightdm/
-
+sudo systemctl set-default graphical.target
