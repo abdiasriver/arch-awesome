50d68da744657332a0e8136cd0ff071ca8e8811e
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-09-27 08:02:37 -0500

    agregado tap-to-click to autostart.sh

--- awesome/awconf/autostart.sh
+++ awesome/awconf/autostart.sh
@@ -16,10 +16,14 @@ run "xfce4-power-manager"
 run "blueberry-tray"
 run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
 run "numlockx on"
-#run "setxkbmap -layout latam"
+run "setxkbmap -layout latam"
+###synaptics tap to click on terminal do this and copy your device to synaptics.sh
+###  sudo libinput list-devices
+run "./.config/awesome/synaptics.sh"
+#run "sudo openvpn --config /etc/openvpn/openvpn.conf"
 #run "volumeicon"
 #run "nitrogen --restore"
-run "conky -c $HOME/.config/awesome/system-overview"
+#run "conky -c $HOME/.config/awesome/system-overview"
 
 #run applications from startup
 #run "firefox"
