7b1508eabf10be4e05c185cd9d05ffe867574e72
abdiasriver <abdias@alumnos.uaslp.edu.mx>
2018-09-27 19:03:21 -0500

    actualizado sept

--- awesome/000-add-user.sh
+++ awesome/000-add-user.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-
+echo "################################################################"
+echo "####             AGREGAR USUARIO CAMBIA EL NOMBRE abdias por el tuyo           ####"
+echo "####                                                    ####"
+echo "################################################################"
 echo "####         3.4---Agregando USUARIO              ####"
 useradd -m -g users -G rfkill,wheel,network,lp,storage,power,video,audio,lp -s /bin/bash abdias
 echo "####         3.5---Modificar el PASSWORD          ####"
